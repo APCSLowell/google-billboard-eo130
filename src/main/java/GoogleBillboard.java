4daa7f283a5120d3cead49a2374b68fcc8cbac2b
eo130 <85082832+eo130@users.noreply.github.com>
2024-01-30 12:45:43 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -4,11 +4,11 @@ public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
 		for(int i = 0; i<e.length()-10; i++){
-    String digits = e.substring(i,10+i);
-    double dNum = Double.parseDouble(digits);
+    String dNum = e.substring(i,10+i);
+    double dNum = Double.parseDouble(dNum);
     boolean cat = isPrime(dNum);
     if(cat == true){
-    System.out.print(digits);
+    System.out.print(dNum);
     break;
     }
     }
